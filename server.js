a36d71c988a50317c3c3a1cd2e6b6cc4b93a2290
JSin <jason.c.sin@gmail.com>
2022-09-26 15:46:48 -0600

    created server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// mount middleware for json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// mount middleware for routes
+app.use('/api/notes', require('./routes/apiRoutes'));
+app.use('/', require('./routes/htmlRoutes'));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
